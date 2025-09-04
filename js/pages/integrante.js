c52cb398577468620733e3084d84a129c749c0d6
Facundo Azcue <fazcue@gmail.com>
2025-09-04 04:48:44 -0300

    Update to use Title component

--- js/pages/integrante.js
+++ js/pages/integrante.js
@@ -1,4 +1,5 @@
 import { members } from '../data/members.js'
+import { Title } from '../components/Title.js'
 
 // get params from url
 const params = new URLSearchParams(window.location.search)
@@ -9,24 +10,21 @@ const currentMember = getCurrentMember(slug)
 
 // update data (title + h1 + message)
 const main = document.getElementsByTagName('main')[0]
-const h1 = document.createElement('h1')
 const p = document.createElement('p')
 
-if (currentMember) {
-	const title = `Integrante ${currentMember.name}`
+let title = ''
 
-	document.title = title
-	h1.textContent = title
+if (currentMember) {
+	title = `Integrante ${currentMember.name}`
 	p.textContent = `Hola, mi fecha de nacimiento es el ${currentMember.birthdate}`
 } else {
-	const title = `Integrante inexistente`
-
-	document.title = title
-	h1.textContent = title
+	title = `Integrante inexistente`
 	p.textContent = ':('
 }
 
-main.append(h1)
+document.title = title
+Title('h1', title)
+
 main.append(p)
 
 /* helper functions */
