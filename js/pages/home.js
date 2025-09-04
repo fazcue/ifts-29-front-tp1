bbba7f123b969bed9adaf7c8b124ef1700fa5899
Facundo Azcue <fazcue@gmail.com>
2025-09-04 04:46:58 -0300

    Update with Title & Description as components

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,8 @@
+import { Title } from '../components/Title.js'
 import { MembersList } from '../components/MembersList.js'
+import { Description } from '../components/Description.js'
 
+Title('h1', 'Grupo 5')
+Description('Descripción grupo 5')
+Title('h2', 'Integrantes')
 MembersList()
