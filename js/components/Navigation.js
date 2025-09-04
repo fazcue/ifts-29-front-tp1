300c96b03e67cf905ec3a0c5ac4121d4f479d82d
Facundo Azcue <fazcue@gmail.com>
2025-09-03 21:03:02 -0300

    Add Navigation component

---
+++ js/components/Navigation.js
@@ -0,0 +1,25 @@
+import { navLinks } from '../data/site.js'
+
+function Navigation() {
+	const nav = document.createElement('nav')
+
+	// list with elements
+	const ul = document.createElement('ul')
+
+	navLinks.forEach((link) => {
+		const li = document.createElement('li')
+		const a = document.createElement('a')
+
+		a.href = link.href
+		a.textContent = link.title
+
+		li.append(a)
+		ul.append(li)
+	})
+
+	nav.append(ul)
+
+	return nav
+}
+
+export { Navigation }
