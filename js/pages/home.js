beb9c476f9f93e2bec7472e1744a4c5d6a84d7ff
Emi-nz <emiliano.nz03@gmail.com>
2025-09-11 21:20:17 -0300

    Update home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -3,6 +3,6 @@ import { MembersList } from '../components/member/MembersList.js'
 import { Description } from '../components/Description.js'
 
 Title('h1', 'Grupo 5')
-Description('Descripción grupo 5')
+Description('Somos un grupo de estudiantes de la carrera de Desarrollo de Sistemas de Software. Nuestro proposito es aplicar en este proyecto los conocimientos obtenidos de la materia.')
 Title('h2', 'Integrantes')
 MembersList()
